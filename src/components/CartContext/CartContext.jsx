c6bc9e5c6b3d677eb7cafb519b247087249417af
Diego Rezende <fdcrezende@gmail.com>
2024-10-14 20:46:17 -0300

    Ajustando as pages

--- src/components/CartContext/CartContext.jsx
+++ src/components/CartContext/CartContext.jsx
@@ -1,79 +1,79 @@
-import { createContext, useState, useReducer } from "react";
+// import { createContext, useState, useReducer } from "react";
 
-const CartContext = createContext([]);
+// const CartContext = createContext([]);
 
-import React from "react";
+// import React from "react";
 
-const cartReducer = (cart, action) => {
-    switch (action.type) {
-      case "addItem": {
-        let newCart;
+// const cartReducer = (cart, action) => {
+//     switch (action.type) {
+//       case "addItem": {
+//         let newCart;
   
-        // Checar se o item já existe no carrinho
-        const existeItem = cart.some(
-          (productItem) => productItem.id === action.item.id
-        );
+//         // Checar se o item já existe no carrinho
+//         const existeItem = cart.some(
+//           (productItem) => productItem.id === action.item.id
+//         );
   
-        if (!existeItem) {
-          newCart = [...cart, action.item];
-        }
-        if (existeItem) {
-          // Licao de casa! Alterar a quantidade de items do carrinho adicionando a quantidade vinda do action.item
-        }
+//         if (!existeItem) {
+//           newCart = [...cart, action.item];
+//         }
+//         if (existeItem) {
+//           // Licao de casa! Alterar a quantidade de items do carrinho adicionando a quantidade vinda do action.item
+//         }
   
-        console.log({ existeItem });
+//         console.log({ existeItem });
   
-        localStorage.setItem("cart", JSON.stringify(newCart));
-        return newCart;
-      }
+//         localStorage.setItem("cart", JSON.stringify(newCart));
+//         return newCart;
+//       }
   
-      case "removeItem": {
-        const filteredCart = cart.filter(
-          (product) => product.id !== action.productId
-        );
-        localStorage.setItem("cart", JSON.stringify(filteredCart));
-        return filteredCart;
-      }
+//       case "removeItem": {
+//         const filteredCart = cart.filter(
+//           (product) => product.id !== action.productId
+//         );
+//         localStorage.setItem("cart", JSON.stringify(filteredCart));
+//         return filteredCart;
+//       }
   
-      case "changeItemQuantity": {
-        const mapperCart = cart.map((product) => {
-          if (product.id !== action.product.id) {
-            return product;
-          }
-          if (product.id === action.product.id) {
-            return {
-              ...product,
-              quantity: action.product.newQuantity,
-            };
-          }
-        });
+//       case "changeItemQuantity": {
+//         const mapperCart = cart.map((product) => {
+//           if (product.id !== action.product.id) {
+//             return product;
+//           }
+//           if (product.id === action.product.id) {
+//             return {
+//               ...product,
+//               quantity: action.product.newQuantity,
+//             };
+//           }
+//         });
   
-        localStorage.setItem("cart", JSON.stringify(mapperCart));
-        return mapperCart;
-      }
+//         localStorage.setItem("cart", JSON.stringify(mapperCart));
+//         return mapperCart;
+//       }
   
-      default:
-        break;
-    }
-  };
+//       default:
+//         break;
+//     }
+//   };
   
-  const initiliazeState = () => {
-    return JSON.parse(localStorage.getItem("cart"));
-  };
+//   const initiliazeState = () => {
+//     return JSON.parse(localStorage.getItem("cart"));
+//   };
   
-  export function CartProvider({ children }) {
-    const [cart, dispatch] = useReducer(cartReducer, initiliazeState() || []);
+//   export function CartProvider({ children }) {
+//     const [cart, dispatch] = useReducer(cartReducer, initiliazeState() || []);
   
-    return (
-      <CartContext.Provider
-        value={{
-          cart,
-          dispatch,
-        }}
-      >
-        {children}
-      </CartContext.Provider>
-    );
-  }
+//     return (
+//       <CartContext.Provider
+//         value={{
+//           cart,
+//           dispatch,
+//         }}
+//       >
+//         {children}
+//       </CartContext.Provider>
+//     );
+//   }
   
-  export default CartContext;
+//   export default CartContext;
